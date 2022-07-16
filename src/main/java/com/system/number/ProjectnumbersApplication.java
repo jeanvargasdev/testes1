e8aa78efa2357cd0f25b405691cf0fc36e1d220e
Jean Vargas <contato@jeanvargas.com.br>
2022-07-15 21:28:56 -0300

    ggg

---
+++ src/main/java/com/system/number/ProjectnumbersApplication.java
@@ -0,0 +1,22 @@
+package com.system.number;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ProjectnumbersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectnumbersApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+
+}
---
+++ src/test/java/com/system/number/ProjectnumbersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.system.number;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectnumbersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
